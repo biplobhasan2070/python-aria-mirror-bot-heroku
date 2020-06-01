404e2877ca650d4135a44afb7a165c2c0a18ab3a
sam01ownloader <66274051+sam01ownloader@users.noreply.github.com>
2020-06-02 02:08:38 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,7 +1,4 @@
 export MAX_DOWNLOAD_SPEED=0
-export MAX_CONCURRENT_DOWNLOADS=3
-aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
-  --max-connection-per-server=10 --rpc-max-request-size=1024M \
-  --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 \
-   --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
-   --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS
+export MAX_CONCURRENT_DOWNLOADS=10
+aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port $RPC_LISTEN_PORT --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS --max-connection-per-server=10 --rpc-max-request-size=1024M --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 --rpc-secret=$ARIA_RPC_SECRET --bt-stop-timeout=600 --max-overall-upload-limit=1 --daemon=true
+echo "Aria2c daemon started"
